050a6b7ae3d1c9fbfd091107454457a6dbec288f
Anitha Goudo <goudoanitha79@gmail.com>
2026-05-29 21:59:20 +0530

    ProductCard.jsx

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -21,7 +21,7 @@ function ProductCard({ product, addToCart }) {
     >
 
       <img
-        src={`http://ecommerce-project-qvh0.onrender.com/uploads/${product.image}`}
+        src={`https://ecommerce-project-qvh0.onrender.com/uploads/${product.image}`}
         alt={product.name}
         style={{
           width: "100%",
