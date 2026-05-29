5db205b85c334a7cd68861ca5484ebc5faf74c29
Anitha Goudo <goudoanitha79@gmail.com>
2026-05-29 21:16:39 +0530

    AdminProducts.jsx

--- client/src/pages/AdminProducts.jsx
+++ client/src/pages/AdminProducts.jsx
@@ -19,7 +19,7 @@ function AdminProducts() {
   const fetchProducts = () => {
 
     axios
-      .get("http://localhost:5000/products")
+      .get("http://https://ecommerce-project-qvh0.onrender.com/products")
       .then((response) => {
 
         setProducts(response.data);
@@ -38,7 +38,7 @@ function AdminProducts() {
     try {
 
       await axios.delete(
-        `http://localhost:5000/delete-product/${id}`
+        `http://https://ecommerce-project-qvh0.onrender.com/delete-product/${id}`
       );
 
       fetchProducts();
@@ -138,7 +138,7 @@ function AdminProducts() {
             >
 
               <img
-                src={`http://localhost:5000/uploads/${product.image}`}
+                src={`http://https://ecommerce-project-qvh0.onrender.com/uploads/${product.image}`}
                 alt={product.name}
                 style={{
                   width: "100%",
