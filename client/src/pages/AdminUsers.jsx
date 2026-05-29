aa0a65cdf6d4f9551969c83625972561d7361876
Anitha Goudo <goudoanitha79@gmail.com>
2026-05-29 21:55:13 +0530

    AdminUsers.jsx

--- client/src/pages/AdminUsers.jsx
+++ client/src/pages/AdminUsers.jsx
@@ -13,7 +13,7 @@ function AdminUsers() {
   useEffect(() => {
 
     axios
-      .get("http://https://ecommerce-project-qvh0.onrender.com/users")
+      .get("http://ecommerce-project-qvh0.onrender.com/users")
       .then((response) => {
 
         setUsers(response.data);
