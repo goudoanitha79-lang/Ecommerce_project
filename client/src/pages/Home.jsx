6a44136d72a4139960b0f8c252cd285fa32695d0
Anitha Goudo <goudoanitha79@gmail.com>
2026-05-29 22:22:01 +0530

    Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
   useEffect(() => {
 
     axios
-      .get("http://ecommerce-project-qvh0.onrender.com/products")
+      .get("https://ecommerce-project-qvh0.onrender.com/products")
       .then((response) => {
 
         setProducts(response.data);
