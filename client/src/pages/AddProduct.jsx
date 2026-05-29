2e0a62d4c7c38ab611cf19a1a58aee23bafc888c
Anitha Goudo <goudoanitha79@gmail.com>
2026-05-29 21:53:49 +0530

    AddProduct.jsx

--- client/src/pages/AddProduct.jsx
+++ client/src/pages/AddProduct.jsx
@@ -34,7 +34,7 @@ function AddProduct() {
 
       const response = await axios.post(
 
-        "http://https://ecommerce-project-qvh0.onrender.com/add-product",
+        "http://ecommerce-project-qvh0.onrender.com/add-product",
 
         formData
 
