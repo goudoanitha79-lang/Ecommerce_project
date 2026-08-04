a32abc2127eb42ee1df32c8195fb1a6d1ef01bb3
Anitha Goudo <goudoanitha79@gmail.com>
2026-08-04 10:49:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,5 +1,15 @@
 // ---------- LOGIN ----------
-app.post("/login", (req, res) => {
+require("dotenv").config();
+
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+const multer = require("multer");
+const path = require("path");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const app = express(); {
   const { email, password } = req.body;
 
   if (!email || !password) {
